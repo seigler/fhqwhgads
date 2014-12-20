adae1b71a7ffc2a3669eed4a04e692f4f8ccc440
Joshua Seigler <git@joshua.seigler.net>
2014-12-20 14:35:45 -0500

    added fake replies of "command not found"

--- build/application.js
+++ build/application.js
@@ -1,4 +1,16 @@
 (function() {
+    function escapeHTML(html) {
+        var fn = function(tag) {
+            var charsToReplace = {
+                "&": "&amp;",
+                "<": "&lt;",
+                ">": "&gt;",
+                '"': "&#34;"
+            };
+            return charsToReplace[tag] || tag;
+        };
+        return html.replace(/[&<>"]/g, fn);
+    }
     function syncTyping() {
         var beforeSelection, selection, afterSelection;
         beforeSelection = this.value.slice(0, this.selectionStart);
@@ -12,4 +24,13 @@
     commandline.onkeydown = syncTyping;
     commandline.onkeyup = syncTyping;
     commandline.onselect = syncTyping;
+    function handleForm() {
+        var scrollback = document.getElementById("scrollback");
+        var val = this.command.value;
+        this.command.value = "";
+        document.getElementById("typing").innerHTML = "";
+        scrollback.innerHTML = scrollback.innerHTML + "a>" + escapeHTML(val) + "<br>Command not found.<br>";
+        return false;
+    }
+    document.forms[0].onsubmit = handleForm;
 })();
--- build/index.php
+++ build/index.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 		<title>FHQWHGADS</title>
+		<link rel="icon" href="data:;base64,iVBORw0KGgo=">
 		<style><?php include('compiled-styles.css'); ?></style>
 	</head>
 	<body>
@@ -12,7 +13,7 @@
 				<div id="scrollback">
 					i love u<br>-fhqwhgadshgnsdhjsdbkhsdabkfabkveybvf<br><br>
 				</div>
-				<div id="commandLine">a&gt;<input id="command" type="text" value="" /><span id="typing"></span></div>
+				<div id="commandLine">a&gt;<input id="command" name="command" type="text" value="" /><span id="typing"></span></div>
 			</label>
 		</form>
 		<script type="application/javascript"><?php include('application.js'); ?></script>
